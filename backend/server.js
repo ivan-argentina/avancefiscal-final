85d064f56064e47c8c543e6ea2c2dab47593f7f6
Ivan Genari <ivan.genari@garciahnos.com.ar>
2026-06-26 15:40:24 -0300

    aumenta limite payload email pdf

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb", extended: true }));
 
 const prepararFacturaFiscal = (factura) => {
   const empresa = factura.empresas;
