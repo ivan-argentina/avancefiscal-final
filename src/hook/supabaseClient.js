cf73d386ab0a3e6bbb01b1a82f1728324c87987f
Ivan Genari <ivan.genari@garciahnos.com.ar>
2026-07-24 16:53:25 -0300

    Mejoras login y recuperación de contraseña saque consoles

--- src/hook/supabaseClient.js
+++ src/hook/supabaseClient.js
@@ -3,8 +3,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("SUPABASE URL:", supabaseUrl);
-console.log("SUPABASE KEY CARGADA:", !!supabaseKey);
-console.log("PRIMEROS 10 CARACTERES:", supabaseKey?.substring(0, 10));
-
 export const supabase = createClient(supabaseUrl, supabaseKey);
